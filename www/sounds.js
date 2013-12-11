58e9056feb90972c1f9f33c3104703b9142986c7
khalitovsv <khalitovsv@gmail.com>
2013-12-11 14:19:53 +0600

    Create sounds.js

---
+++ www/sounds.js
@@ -0,0 +1,9 @@
+cordova.define("org.apache.cordova.sounds.Sounds", function(require, exports, module) {
+	var exec = require('cordova/exec');
+	var sounds = {
+	    tick:function() {
+	        exec(null, null, "Sounds", "tick", []);
+	    }
+	};
+	module.exports = sounds;
+});
