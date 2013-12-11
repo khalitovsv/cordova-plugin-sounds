a2afe39c297ce3ae8407089a2d2d50705953390a
khalitovsv <khalitovsv@gmail.com>
2013-12-11 16:26:37 +0600

    Update Sounds.java

--- src/android/Sounds.java
+++ src/android/Sounds.java
@@ -3,20 +3,50 @@ package org.apache.cordova.sounds;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
+import org.json.JSONException;
+
 import android.media.AudioManager;
 
 public class Sounds extends CordovaPlugin {
 	private AudioManager audioManager = null;
-	 
+	
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
 		if(this.audioManager==null) {
 			this.audioManager = (AudioManager) this.cordova.getActivity().getSystemService("audio");
 		}
 		
-		if (action.equals("tick")) {
+		if (action.equals("click")) {
 			this.audioManager.playSoundEffect(AudioManager.FX_KEY_CLICK);
-		} else {
+		} else
+		if (action.equals("delete")) {
+			this.audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_DELETE);
+		} else
+		if (action.equals("invalid") && android.os.Build.VERSION.SDK_INT>=19) {
+			this.audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_INVALID);
+		} else
+		if (action.equals("return")) {
+			this.audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_RETURN);
+		} else
+		if (action.equals("spacebar")) {
+			this.audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR);
+		} else
+		if (action.equals("standard")) {
+			this.audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD);
+		} else
+		if (action.equals("down")) {
+			this.audioManager.playSoundEffect(AudioManager.FX_FOCUS_NAVIGATION_DOWN);
+		} else
+		if (action.equals("left")) {
+			this.audioManager.playSoundEffect(AudioManager.FX_FOCUS_NAVIGATION_LEFT);
+		} else
+		if (action.equals("right")) {
+			this.audioManager.playSoundEffect(AudioManager.FX_FOCUS_NAVIGATION_RIGHT);
+		} else
+		if (action.equals("up")) {
+			this.audioManager.playSoundEffect(AudioManager.FX_FOCUS_NAVIGATION_UP);
+		} else
+		{
 			return false;
 		}
 
