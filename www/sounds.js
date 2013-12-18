9d746354181462778fd328cc0594dfa9912e8e87
khalitovsv <khalitovsv@gmail.com>
2013-12-18 23:08:04 +0600

    Update sounds.js

--- www/sounds.js
+++ www/sounds.js
@@ -1,7 +1,34 @@
 var exec = require('cordova/exec');
 var sounds = {
-	tick:function() {
-		exec(null, null, "Sounds", "tick", []);
+	'click': function() {
+		exec(null, null, 'Sounds', 'click', []);
+	},
+	'delete': function() {
+		exec(null, null, 'Sounds', 'delete', []);
+	},
+	'invalid': function() {
+		exec(null, null, 'Sounds', 'invalid', []);
+	},
+	'return': function() {
+		exec(null, null, 'Sounds', 'return', []);
+	},
+	'spacebar': function() {
+		exec(null, null, 'Sounds', 'spacebar', []);
+	},
+	'standard': function() {
+		exec(null, null, 'Sounds', 'standard', []);
+	},
+	'down': function() {
+		exec(null, null, 'Sounds', 'down', []);
+	},
+	'left': function() {
+		exec(null, null, 'Sounds', 'left', []);
+	},
+	'right': function() {
+		exec(null, null, 'Sounds', 'right', []);
+	},
+	'up': function() {
+		exec(null, null, 'Sounds', 'up', []);
 	}
 };
 module.exports = sounds;
